c37a7f7f2a053da748da7f90a70740c0763afccb
Echelon101 <timomrx1@gmail.com>
2017-06-21 13:40:43 +0200

    test code

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+$bund = array("Baden-Württemberg","Bayern","Berlin","Brandenburg","Bremen","Hamburg","Hessen","Mecklenburg-Vorpommern", "Niedersachsen","Nordrhein-Westfalen","Rheinland-Pfalz","Saarland","Sachsen","Sachsen-Anhalt","Schleswig-Holstein","Thüringen");
+echo '<pre>';
+foreach ($bund as $entry){
+	echo $entry."<br>";
+}
+echo '</pre>';
