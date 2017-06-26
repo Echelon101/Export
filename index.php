3fd2673e2477eb21500441acba5ad974d6d40acd
Echelon101 <timomrx1@gmail.com>
2017-06-26 17:00:01 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exporter</title>
+</head>
+<body>
+	<a href="Pullinfo.php">AdressExport Deutschlandweit</a><br>
+	<a href="PullinfoBund.php">AdressExport geteilt in Bundesländern</a>
+</body>
+</html>
