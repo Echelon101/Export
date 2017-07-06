9735985eae8556b10a9f56774765774c56040f1d
Echelon101 <timomrx1@gmail.com>
2017-07-06 17:04:40 +0200

    Added charset utf8mb4 and added convert for Umlaute

--- sites/export.php
+++ sites/export.php
@@ -19,109 +19,111 @@ try {
  */
 function QueryExport($search1, $search2, $search3, $searchName, $searchIndex){
 	try{
-	try{
-	$db = new PDO('mysql:host=localhost;dbname=adressen', 'root', '');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	/*
-	 * Declaration Section
-	 */
-	$search1 = $search1."%";
-	$search2 = $search2."%";
-	$search3 = $search3."%";
-	
-	/*
-	 * End Declaration Section
-	 */
-	$headers = '
-	SELECT * FROM adressen LIMIT 0,1;
-	';
-	
-	$sql = '
-	SELECT *
-	FROM adressen
-	WHERE wz2003_1 LIKE ? OR wz2003_2 LIKE ? OR wz2003_3 LIKE ?
-	';
-	if(!isset($_SESSION['theaders'])){
-		$theaders_stmnt = $db->prepare($headers);
-		$theaders_result = $theaders_stmnt->execute();
-		$theaders = $theaders_stmnt->fetch(PDO::FETCH_ASSOC);
-		$_SESSION['theaders'] = array_keys($theaders);
-	}
-	$saveset = array();
-	$stmnt = $db->prepare($sql);
-	$result = $stmnt->execute(array("$search1", "$search2", "$search3"));
-	
-	$file = "Export_$searchName.txt";
-	$path = "../export/";
-	$logpath = "../export/log/";
-	$logfn = "log_Export_".$searchName."_".$searchIndex."_".$search1.".log";
-	$export = fopen($path.$file, 'w');
-	//$log = fopen($logpath.$logfn, 'w');
-	$enclosure = '"';
-	$delimiter = ';';
-	
-	while ($fetch = $stmnt->fetch(PDO::FETCH_ASSOC)){
-		array_push($saveset, array_values($fetch));
-	}
-	fputcsv($export, $_SESSION['theaders'], $delimiter, $enclosure);
-	//fwrite($log, print_r($_SESSION['theaders'], true));
-	foreach ($saveset as $entry){
-		fputcsv($export, $entry, $delimiter, $enclosure);
-		//fwrite($log, print_r($entry, true));
+		try{
+			$db = new PDO('mysql:host=localhost;dbname=adressen;charset=utf8mb4', 'root', '');
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$db->exec("set names utf8mb4");
+			/*
+			 * Declaration Section
+			 */
+			$search1 = $search1."%";
+			$search2 = $search2."%";
+			$search3 = $search3."%";
+			$searchNameN = clear_string($searchName);
+			
+			/*
+			 * End Declaration Section
+			 */
+			$headers = '
+			SELECT * FROM adressen LIMIT 0,1;
+			';
+			
+			$sql = '
+			SELECT *
+			FROM adressen
+			WHERE wz2003_1 LIKE ? OR wz2003_2 LIKE ? OR wz2003_3 LIKE ?
+			';
+			if(!isset($_SESSION['theaders'])){
+				$theaders_stmnt = $db->prepare($headers);
+				$theaders_result = $theaders_stmnt->execute();
+				$theaders = $theaders_stmnt->fetch(PDO::FETCH_ASSOC);
+				$_SESSION['theaders'] = array_keys($theaders);
+			}  
+			$saveset = array();
+			$stmnt = $db->prepare($sql);
+			$result = $stmnt->execute(array("$search1", "$search2", "$search3"));
+			$file = "export_$searchNameN.txt";
+			$path = "../export/";
+			$logpath = "../export/log/";
+			$logfn = "log_Export_".$searchNameN."_".$searchIndex."_".$search1.".log";
+			$export = fopen($path.$file, 'w');
+			//$log = fopen($logpath.$logfn, 'w');
+			$enclosure = '"';
+			$delimiter = ';';
+
+			while ($fetch = $stmnt->fetch(PDO::FETCH_ASSOC)){
+				array_push($saveset, array_values($fetch));
+			}
+			fputcsv($export, $_SESSION['theaders'], $delimiter, $enclosure);
+			//fwrite($log, print_r($_SESSION['theaders'], true));
+			foreach ($saveset as $entry){
+				fputcsv($export, $entry, $delimiter, $enclosure);
+				//fwrite($log, print_r($entry, true));
+				}
+			//$data = fread($export, filesize($path.$file));
+			fclose($export);
+			//fclose($log);
+			//$fname = $path.$file.'.gz';
+			//$zp = gzopen($fname, "w9");
+			//gzwrite($zp, $data);
+			//gzclose($zp);
+			return true;
+		}catch (PDOException $pdoE){
+			echo $pdoE->getMessage(); 
 		}
-		//$data = fread($export, filesize($path.$file));
-		fclose($export);
-		//fclose($log);
-		//$fname = $path.$file.'.gz';
-		//$zp = gzopen($fname, "w9");
-		//gzwrite($zp, $data);
-		//gzclose($zp);
-	return true;
-	}catch (PDOException $pdoE){
-		echo $pdoE->getMessage(); 
-	}
 	}catch (Exception $e){
 		echo $e->getMessage();
 	}
 }
 function SubExport($search1, $search2, $search3, $searchName, $searchIndex){
 	try{
-	$dbcon = new PDO('mysql:host=localhost;dbname=adressen;charset=utf8', 'root', '');
-	$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$bundsave = array();
-	$search1 = $search1."%";
-	$search2 = $search2."%";
-	$search3 = $search3."%";
-	$sql = '
-	SELECT *
-	FROM adressen
-	WHERE Bundesland = ? AND (wz2003_1 LIKE ? OR wz2003_2 LIKE ? OR wz2003_3 LIKE ?)
-	';
-	$i=0;
-	if (strlen($search1)<=3 && strlen($search2) <=3 && strlen($search3) <= 3){
-		$bund = array("Baden-Württemberg","Bayern","Berlin","Brandenburg","Bremen","Hamburg","Hessen","Mecklenburg-Vorpommern", "Niedersachsen","Nordrhein-Westfalen","Rheinland-Pfalz","Saarland","Sachsen","Sachsen-Anhalt","Schleswig-Holstein","Thüringen");
-		$bund_short = array("Baden-Württemberg", "Thüringen");
-		foreach ($bund_short as $index){
-			$getbund_statement = $dbcon->prepare($sql);
-			$getbund_result = $getbund_statement->execute(array("$index", "$search1", "$search2", "$search3"));
-			while ($getbund_fetch = $getbund_statement->fetch(PDO::FETCH_ASSOC)){
-				array_push($bundsave, array_values($getbund_fetch));
+		$dbcon = new PDO('mysql:host=localhost;dbname=adressen;charset=utf8', 'root', '');
+		$dbcon->exec("set names utf8");
+		$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$bundsave = array();
+		$search1 = $search1."%";
+		$search2 = $search2."%";
+		$search3 = $search3."%";
+		$sql = '
+		SELECT *
+		FROM adressen
+		WHERE Bundesland = ? AND (wz2003_1 LIKE ? OR wz2003_2 LIKE ? OR wz2003_3 LIKE ?)
+		';
+		$i=0;
+		if (strlen($search1)<=3 && strlen($search2) <=3 && strlen($search3) <= 3){
+			$bund = array("Baden-Württemberg","Bayern","Berlin","Brandenburg","Bremen","Hamburg","Hessen","Mecklenburg-Vorpommern", "Niedersachsen","Nordrhein-Westfalen","Rheinland-Pfalz","Saarland","Sachsen","Sachsen-Anhalt","Schleswig-Holstein","Thüringen");
+			$bund_short = array("Baden-Württemberg", "Thüringen");
+			foreach ($bund_short as $index){
+				$getbund_statement = $dbcon->prepare($sql);
+				$getbund_result = $getbund_statement->execute(array("$index", "$search1", "$search2", "$search3"));
+				while ($getbund_fetch = $getbund_statement->fetch(PDO::FETCH_ASSOC)){
+					array_push($bundsave, array_values($getbund_fetch));
+				}
+				$file = "Export_".$searchName."_".$index.".txt";
+				$path = "../exportbund/";
+				$logpath = "../exportbund/log/";
+				$logfn = "log_Export_".$searchName."_".$searchIndex."_".$search1."_".$index.".log";
+				$export = fopen($path.$file, 'w');
+				$log = fopen($logpath.$logfn, 'w');
+				foreach ($bundsave as $exportentry){
+					fputcsv($export, $exportentry);
+					//fwrite($log, print_r($exportentry, true));
+				}
 			}
-			$file = "Export_".$searchName."_".$index.".txt";
-			$path = "../exportbund/";
-			$logpath = "../exportbund/log/";
-			$logfn = "log_Export_".$searchName."_".$searchIndex."_".$search1."_".$index.".log";
-			$export = fopen($path.$file, 'w');
-			$log = fopen($logpath.$logfn, 'w');
-			foreach ($bundsave as $exportentry){
-				fputcsv($export, $exportentry);
-				//fwrite($log, print_r($exportentry, true));
-			}
+			return true;
+		}else{
+			return false;
 		}
-		return true;
-	}else{
-		return false;
-	}
 	}catch (PDOException $pdoError){
 		echo $pdoError->getMessage();
 	}
@@ -130,6 +132,31 @@ function SubExport($search1, $search2, $search3, $searchName, $searchIndex){
 	echo "Error: " . $e->getMessage() . "<br>";
 	return false;
 	die();
+}
+function clear_string($str, $how = '-'){
+	$search = array("ä", "ö", "ü", "ß", "Ä", "Ö",
+			"Ü", "&", "é", "á", "ó",
+			" :)", " :D", " :-)", " :P",
+			" :O", " ;D", " ;)", " ^^",
+			" :|", " :-/", ":)", ":D",
+			":-)", ":P", ":O", ";D", ";)",
+			"^^", ":|", ":-/", "(", ")", "[", "]",
+			"<", ">", "!", "\"", "§", "$", "%", "&",
+			"/", "(", ")", "=", "?", "`", "´", "*", "'",
+			"_", ":", ";", "²", "³", "{", "}",
+			"\\", "~", "#", "+", ".", ",",
+			"=", ":", "=)");
+	$replace = array("ae", "oe", "ue", "ss", "Ae", "Oe",
+			"Ue", "und", "e", "a", "o", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "", "");
+	$str = str_replace($search, $replace, $str);
+	$str = strtolower(preg_replace("/[^a-zA-Z0-9]+/", trim($how), $str));
+	return $str;
 }
 
 
--- sites/test.php
+++ sites/test.php
@@ -4,3 +4,35 @@ include '../config/config.php';
 echo '<pre>';
 print_r($_SESSION);
 echo '</pre>';
+$trimmer = trim("-");
+echo $trimmer."<br>";
+$input = "Ämter und Ärzte und Bauernhöfe";
+$trimmed = clear_string($input);
+
+function clear_string($str, $how = '-'){
+	$search = array("ä", "ö", "ü", "ß", "Ä", "Ö",
+			"Ü", "&", "é", "á", "ó",
+			" :)", " :D", " :-)", " :P",
+			" :O", " ;D", " ;)", " ^^",
+			" :|", " :-/", ":)", ":D",
+			":-)", ":P", ":O", ";D", ";)",
+			"^^", ":|", ":-/", "(", ")", "[", "]",
+			"<", ">", "!", "\"", "§", "$", "%", "&",
+			"/", "(", ")", "=", "?", "`", "´", "*", "'",
+			"_", ":", ";", "²", "³", "{", "}",
+			"\\", "~", "#", "+", ".", ",",
+			"=", ":", "=)");
+	$replace = array("ae", "oe", "ue", "ss", "Ae", "Oe",
+			"Ue", "und", "e", "a", "o", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "",
+			"", "", "", "", "", "", "", "", "", "");
+	$str = str_replace($search, $replace, $str);
+	echo $str.'<br>';
+	$str = strtolower(preg_replace("/[^a-zA-Z0-9]+/", trim($how), $str));
+	echo $str.'<br>';
+	return $str;
+}
