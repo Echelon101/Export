24dfa814af39845b2d34ab66ee36d0856ce94db8
Echelon101 <timomrx1@gmail.com>
2017-07-12 12:13:26 +0200

    added rowcount scripts to index

--- index.php
+++ index.php
@@ -9,5 +9,8 @@ session_start();
 <body>
 	<a href="sites/komplett.php">AdressExport Deutschlandweit</a><br>
 	<a href="sites/filtered.php">AdressExport geteilt in Bundesländern</a>
+	<hr>
+	<a href="sites/gesamtcount.php">RowCount Deutschland</a><br>
+	<a href="sites/filteredcount.php">RowCount geteilt in Bundesländern</a>
 </body>
 </html>
