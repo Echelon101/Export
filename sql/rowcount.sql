9cc0d9b3af79e3fcbc24fce120177fb73501a020
Echelon101 <timomrx1@gmail.com>
2017-07-12 12:14:04 +0200

    added sqwl file for rowcount database

---
+++ sql/rowcount.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `rowcount` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `wz2003` varchar(255) NOT NULL,
+  `rowcount` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
