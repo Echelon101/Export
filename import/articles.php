453b159adeb1b1fdafbfb2686526a6c9297fcaf2
Echelon101 <timomrx1@gmail.com>
2017-08-09 15:50:35 +0200

    implementing support for second list of articles

--- import/articles.php
+++ import/articles.php
@@ -6,9 +6,13 @@ define("path", "../files/");
 define("Article_Prefix", "AD");
 define("tax", "19.00");
 define("supplier", "MaxxDiscount GmbH & Co. KG");
+define("ArtImage", "https://www.address-base.de/images/product_images/info_images/x10_4.jpg.pagespeed.ic._XWEulng58.jpg");
+define("CatAlleBranchen", 6);
+
 $headers = 'SELECT * FROM articles LIMIT 0,1';
 
 $pdo = new PDO("mysql:host=localhost;dbname=testshop;charset=utf8","root","");
+
 $get_Info = $pdo->prepare("SELECT `ID`, `Name`, `rowcount` FROM rowcount");
 $get_InfoResult = $get_Info->execute();
 $saveset = null;
@@ -16,14 +20,56 @@ $saveset = array();
 
 while($get_InfoSave = $get_Info->fetch(PDO::FETCH_ASSOC)){
 	//array_push($saveset, array_values($get_InfoSave));
+	
 	$artNr = Article_Prefix . str_pad($get_InfoSave['ID'], 5,"0", STR_PAD_LEFT);
 	$ek = ceil($get_InfoSave['rowcount']/5);
-	$createArticle = $pdo->prepare("INSERT INTO articles (ordernumber, mainnumber, name, supplier, tax, price_EK) VALUES (:ordernumber, :mainnumber, :name, :supplier, :tax, :price_EK)");
-	$createArticleResult = $createArticle->execute(array("ordernumber" => $artNr, "mainnumber" => $artNr, "name" => $get_InfoSave['Name'], "supplier" => supplier, "tax" => tax, "price_EK" => $ek));
+	
+	$description = "Branche: " . $get_InfoSave['Name'] . ", Datensätze: " . $get_InfoSave['rowcount'];
+	$descriptionLong = "Branche: " . $get_InfoSave['Name'] . ", Datensätze: " . $get_InfoSave['rowcount'];
+	
+	$createArticle = $pdo->prepare("INSERT INTO articles (ordernumber, mainnumber, name, supplier, tax, price_EK, imageUrl, active, description, description_long, categories) VALUES (:ordernumber, :mainnumber, :name, :supplier, :tax, :price_EK, :imageUrl, :active, :description, :description_long, :categories)");
+	$createArticleResult = $createArticle->execute(
+			array(
+					"ordernumber" => $artNr,
+					"mainnumber" => $artNr,
+					"name" => $get_InfoSave['Name'],
+					"supplier" => supplier,
+					"tax" => tax,
+					"price_EK" => $ek,
+					"imageUrl" => ArtImage,
+					"active" => true,
+					"description" => $description,
+					"description_long" => $descriptionLong,
+					//"categories" => $category
+			)
+		);
+	$getFilterdArticle = $pdo->prepare("SELECT * FROM rowcountbund WHERE Name = :Name");
+	$getFilterdArticleR = $getFilterdArticle->execute(array('Name' => $get_InfoSave['Name']));
+	
+	while($getFilterdArticleS = $getFilterdArticle->fetch(PDO::FETCH_ASSOC)){
+		$createFilterdArticle = $pdo->prepare("INSERT INTO articles (ordernumber, mainnumber, name, supplier, tax, price_EK, imageUrl, active, description, description_long, categories) VALUES (:ordernumber, :mainnumber, :name, :supplier, :tax, :price_EK, :imageUrl, :active, :description, :description_long, :categories)");
+		$createFilterdArticleResult = $createFilterdArticle->execute(
+				array(
+						"ordernumber" => $artNr,
+						"mainnumber" => $artNr,
+						"name" => $get_InfoSave['Name'],
+						"supplier" => supplier,
+						"tax" => tax,
+						"price_EK" => $ek,
+						"imageUrl" => ArtImage,
+						"active" => true,
+						"description" => $description,
+						"description_long" => $descriptionLong,
+						//"categories" => $category
+				)
+				);
+	}
 }
 }catch (PDOException $pdoE){
 	echo $pdoE->getMessage();
 }
+
+
 if(!isset($_SESSION['theaders'])){
 	$theaders_stmnt = $pdo->prepare($headers);
 	$theaders_result = $theaders_stmnt->execute();
