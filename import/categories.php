defd69aad27e4b73b6b5c340b2f67cb9cedf5982
Echelon101 <timomrx1@gmail.com>
2017-08-09 15:50:03 +0200

    support for categories

--- import/categories.php
+++ import/categories.php
@@ -1,1 +1,22 @@
 <?php
+session_start();
+
+define("CatOffset", 5);
+define("CatParent", 5);
+
+$pdo = new PDO("mysql:host=localhost;dbname=testshop;charset=utf8", "root", "");
+
+$categories = array('Alle Branchen', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' , 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+$count = 5;
+foreach ($categories as $category){
+	$count++;
+	
+	$newCatS = $pdo->prepare("INSERT INTO categories (categoryId, parentID, description) VALUES (:categoryId, :parentID, :description)");
+	$newCatR = $newCatS->execute(
+			array(
+				"categoryId" => $count,
+				"parentID" => CatParent,
+				"description" => $category,
+			)
+		);
+}
